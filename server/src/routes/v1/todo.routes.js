47e40ec2cb1b6a10a18f37b7c0e8ac65717c0e20
rajeswarinath17 <krishan8974783135@gmail.com>
2026-08-05 23:46:24 +0530

    refactor: v1 routes updated, only get is available for v1 users

--- server/src/routes/v1/todo.routes.js
+++ server/src/routes/v1/todo.routes.js
@@ -4,9 +4,30 @@ const todoRouter = Router();
 
 
 // Todo CRUD routes
-todoRouter.post('/', todoController.createTodo);
 todoRouter.get('/', todoController.getTodos);
-todoRouter.patch('/:id', todoController.updateTodo);
-todoRouter.delete('/:id', todoController.deleteTodo);
+// todoRouter.post('/', todoController.createTodo);
+// todoRouter.patch('/:id', todoController.updateTodo);
+// todoRouter.delete('/:id', todoController.deleteTodo);
+
+todoRouter.post("/", (req, res) => {
+  res.status(405).json({
+    success: false,
+    message: "V1 API is read-only. Use V2 for CRUD operations.",
+  });
+});
+
+todoRouter.patch("/:id", (req, res) => {
+  res.status(405).json({
+    success: false,
+    message: "V1 API is read-only. Use V2 for CRUD operations.",
+  });
+});
+
+todoRouter.delete("/:id", (req, res) => {
+  res.status(405).json({
+    success: false,
+    message: "V1 API is read-only. Use V2 for CRUD operations.",
+  });
+});
 
 export default todoRouter;
