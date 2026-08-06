01ba9d7c24d0fb873bd72f534347986215c86cfc
rajeswarinath17 <krishan8974783135@gmail.com>
2026-08-06 20:45:56 +0530

    fix helmet import

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import Overview from "./docs/Overview";
 import QuickStart from "./docs/QuickStart";
 import DocsSidebar from "../components/docs/DocsSidebar";
